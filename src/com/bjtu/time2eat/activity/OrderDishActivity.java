d5f2b43bfa0509e0df935825256d6d63fa71bb90
李传宝 <lichuanbao.buddy@gmail.com>
2014-05-14 11:55:47 +0800

    删除多余引用

--- src/com/bjtu/time2eat/activity/OrderDishActivity.java
+++ src/com/bjtu/time2eat/activity/OrderDishActivity.java
@@ -1,19 +1,9 @@
 package com.bjtu.time2eat.activity;
 
-import java.util.Calendar;
+import android.app.Activity;
+import android.os.Bundle;
 
 import com.example.time2eat.R;
-
-import android.app.Activity;
-import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.TimePickerDialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.TimePicker;
 
 public class OrderDishActivity extends Activity {
 	
